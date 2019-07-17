0b42b03dd55a9551cd124e6e6d0c2aa4f8cb18aa
Tracey Radcliffe <radcliffe.tracey@gmail.com>
2019-07-17 14:24:58 -0700

    basic youtube search working

---
+++ src/apis/youtube.js
@@ -0,0 +1,12 @@
+'use strict';
+import cred from './credentials.js'
+import axios from 'axios';
+
+export default axios.create({
+  baseURL: 'https://www.googleapis.com/youtube/v3',
+  params: {
+    part: 'snippet',
+    maxResults: 5,
+    key: cred.KEY
+  }
+})
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -9,8 +9,8 @@ class SearchBar extends React.Component {
 
   onFormSubmit = (event) => {
     event.preventDefault();
-
-    // TODO: Call callback from parent component
+    
+    this.props.onFormSubmit(this.state.term);
   };
 
   render() {
